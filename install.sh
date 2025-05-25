63744d0eb836d88929e3de034978b453e960ca7f
Lily Anne <lily.anne.hall@protonmail.com>
2025-05-24 23:47:50 -0700

    omit dev deps in install script

--- install.sh
+++ install.sh
@@ -15,6 +15,6 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 nvm install 22
-npm install -g @tacticalchihuahua/dusk
+npm install -g @tacticalchihuahua/dusk --omit dev
 
 dusk --install --enable-autostart
